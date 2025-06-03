e2ac2c80cfd35ee2fa9cd692db1079e5465d62ee
Tea <tea.tdedic@gmail.com>
2025-06-03 22:43:10 +0200

    Testing Signup and Login

---
+++ Reflectly.Services/Migrations/20250603202027_AddLoginFieldsToUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reflectly.Services.Migrations
+{
+    public partial class AddLoginFieldsToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Reflectly.Services/obj/Debug/net6.0/Reflectly.Services.AssemblyInfo.cs
+++ Reflectly.Services/obj/Debug/net6.0/Reflectly.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reflectly.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4886cb7b49eafc262c39165d63d3f9285ef019")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reflectly.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reflectly.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
